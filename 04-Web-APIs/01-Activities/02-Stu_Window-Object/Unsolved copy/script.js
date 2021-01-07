478e931f0b6584eb306738b32fa5f166973a9c16
dboren <danielboren@att.net>
2021-01-06 22:06:32 -0600

    1_6_21 db Notes

---
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved copy/script.js
@@ -0,0 +1,13 @@
+// TODO: What will the following line of code log?
+console.log(window);
+
+// TODO: What will the following line of code log?
+console.log(document);
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+console.log(document.head);
+
+console.log(document.length);
---
+++ 04-Web-APIs/01-Activities/04-Stu_Traverse-DOM/Unsolved copy/assets/js/script.js
@@ -0,0 +1,9 @@
+// Access element using id
+var articlesDiv = document.getElementById("articles");
+//var mainDiv = document.getElementById("main");
+
+var mainDiv = document.getElementsByTag("h1")
+
+// Change style by accessing style object's properties
+articlesDiv.children[0].style.fontSize = "50px";
+mainDiv.h1.style.color = "white";
