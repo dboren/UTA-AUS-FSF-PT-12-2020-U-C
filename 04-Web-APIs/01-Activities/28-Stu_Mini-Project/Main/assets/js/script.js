f4466c6bb7d5b1b28004f8e9a49828cfd505af35
dboren <danielboren@att.net>
2021-01-11 22:04:30 -0600

    1_11_21 db notes 2

--- 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
+++ 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
@@ -1,1 +1,23 @@
-//javascript code goes here
+
+
+var wordList = ["Iron Man", "Odinson", "Black Widow", "Captain America", "Vision"];
+
+function startRound() {
+    var thisRoundWord = "";
+    //for (var i=0; i < wordList.length; i++) {
+    thisRoundWord = wordList[(Math.floor(Math.random()*wordList.length))]; //}
+    console.log(thisRoundWord);
+    return thisRoundWord;
+
+    console.log(thisRoundWord);
+}
+
+
+
+
+
+
+// var startButton = document.getElementByID("#sb");
+// startButton.addEventListener("click", startRound);
+
+startRound();
