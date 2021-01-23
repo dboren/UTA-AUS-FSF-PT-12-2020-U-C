d8c6888d2898bdcd74c338bc70e904ca421227aa
dboren <danielboren@att.net>
2021-01-23 14:30:02 -0600

    db notes 1_23_21

--- 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved copy/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved copy/assets/js/script.js
@@ -12,21 +12,6 @@ function getApi() {
       // Use the console to examine the response
       console.log(data);
       // TODO: Loop through the data and generate your HTML
-      for (var i = 0; i < 5; i++) {
-        var userName = document.createElement("h3");
-        var userURL = document.createElement("h4");
-        userName.textContent = data[i].login;
-        userURL.textContent = data[i].html_url;
-        userContainer.appendChild(userName);
-        userContainer.appendChild(userURL);
-        console.log(userName);
-        console.log(userURL);
-      }
-
     });
 }
-
-// console.log(userName);
-// console.log(userURL);
-
 fetchButton.addEventListener('click', getApi);
